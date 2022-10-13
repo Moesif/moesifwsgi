1ddf432bd941fcb4b924b6655e99ab85f862c405
xiluan <xinyujie0320@gmail.com>
2022-10-12 20:46:44 -0700

    use moesifpythonreques 0.3.0 for app_config (#53)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.5.1',
+    version='1.6.0',
 
     description='Moesif Middleware for Python WSGI based platforms (Flask, Bottle & Others)',
     long_description=long_description,
@@ -82,7 +82,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests', 'isodatetimehandler', 'moesifapi>=1.4.0', 'moesifpythonrequest>=0.2.0', 'apscheduler'],
+    install_requires=['requests', 'isodatetimehandler', 'moesifapi>=1.4.0', 'moesifpythonrequest>=0.3.0', 'apscheduler'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

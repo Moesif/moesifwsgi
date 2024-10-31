df5843f0fb697e2d77f342cf41161d7c02e8d3fa
Xingheng Wang <xinghengwang@users.noreply.github.com>
2024-10-31 13:48:29 -0700

    #188481396 : use ipaddress package to test valid v6 ipaddress (#72)
    * use ipaddress package to test
    
    * Update setup.py
    
    * cleanup typos etc
    
    ---------
    
    Co-authored-by: Praveen Kumar <praveen@moesif.com>

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.9.6',
+    version='1.9.7',
 
     description='Moesif Middleware for Python WSGI based platforms (Flask, Bottle & Others)',
     long_description=long_description,
@@ -49,7 +49,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
